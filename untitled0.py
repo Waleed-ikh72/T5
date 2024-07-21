5db5f66404794f94bddd36b991a964e41df12368
Waleed-ikh72 <waleed-khwan@hotmail.com>
2024-07-21 20:47:28 +0300

    Update untitled0.py

--- untitled0.py
+++ untitled0.py
@@ -13,11 +13,8 @@ df = pd.DataFrame({'Student' : ['A', 'A', 'A', 'B', 'B', 'C'],
                    'Score' : [15., 23., 17., 5., 8., 12.]})
 print(df, end='\n\n')
 
-#group = df.groupby(['Studnet'], as_index=False)
-opt1 = df.mean("Student",axis=1)
-opt2 = df.groupby("Student").mean()
-opt3 = df.mean("Score",axis=0)
-opt3 = df.mean("Student",axis=0)
+opt1 = df.groupby("Student").mean()
+
 
 
 print(group.mean())
